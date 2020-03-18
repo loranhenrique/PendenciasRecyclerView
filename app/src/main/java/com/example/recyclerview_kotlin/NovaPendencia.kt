181fd982dc6bcaafdc009fb70c99c7c67be2d31f
Loran Henrique <loranteixeira@icloud.com>
2020-03-18 12:21:45 -0300

    Começando novaPendencia

---
+++ app/src/main/java/com/example/recyclerview_kotlin/NovaPendencia.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerview_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovaPendencia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_pendencia)
+    }
+}
